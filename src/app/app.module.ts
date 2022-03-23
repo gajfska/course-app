7674f1bc68ee618dd51601cef7951f4fc384cd80
Weronika Gajska <weronika.gajska@gmail.com>
2022-03-23 20:45:44 +0100

    Added debug devtool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +15,7 @@ import { LoginModule } from './features/login/login.module';
 import { RegistrationModule } from './features/registration/registration.module';
 import { APIInterceptor } from './shared/baseUrl.interceptor';
 import { SharedModule } from './shared/shared.module';
+import { effects, reducers } from './store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +27,13 @@ import { SharedModule } from './shared/shared.module';
     CourseEditModule,
     SharedModule,
     AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot(effects),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    })
   ],
   providers: [
     AuthorizedGuard,
